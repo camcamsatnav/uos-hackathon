353036c3f3f8297fe4726d0f13d23666c5e05a6f
Bhavya Singh <bhavyasingh2611@outlook.com>
2024-03-01 02:07:46 +0000

    brrrrr

--- backend/assign/views.py
+++ backend/assign/views.py
@@ -14,49 +14,62 @@ def dumper(obj):
         return obj.__dict__
 
 
-def student(request):
+def getStudentData(studentID, isStudent=True):
     jsonObj = {}
+    students = Student.objects.filter(clerk_id__exact=studentID)
+    if len(students) == 0:
+        return {"error": "No student with that ID"}
+    projects = students[0].projects.all()
+    projectID = []
+    for project in projects:
+        projectID.append(project.id)
+
+    criteria1 = Q(student__exact=students[0].id)
+    criteria2 = Q(project__in=projectID)
+
+    grades = Grade.objects.filter(criteria1 & criteria2)
+    individualMarks = IndividualMark.objects.filter(criteria1 & criteria2)
+    jsonObj['id'] = students[0].id
+    jsonObj['name'] = students[0].name
+
+    jsonObj['projects'] = []
+    for project in projects:
+        temp = {}
+        tempIndividualMarks = []
+
+        for mark in individualMarks:
+            if mark.project.id == project.id:
+                tempObj = {'mark': mark.mark}
+                if not isStudent:
+                    tempObj['academic'] = mark.academics.clerk_id
+                tempIndividualMarks.append(tempObj)
+
+        for grade in grades:
+            if grade.project.id == project.id:
+                temp['state'] = grade.state
+                temp['grade'] = tempIndividualMarks
+
+                if grade.state not in ['SUBMITTED', 'MODERATED'] and isStudent:
+                    temp['grade'] = []
+
+        temp['id'] = project.id
+        temp['name'] = project.name
+        temp['module'] = project.module.code
+
+        jsonObj['projects'].append(temp)
+    jsonObj['clerk_id'] = students[0].clerk_id
+
+    return jsonObj
+
+
+def student(request):
     try:
         studentID = request.GET['id']
     except:
         return HttpResponse('{"error": "No student ID provided"}')
 
     if studentID != '':
-        students = Student.objects.filter(clerk_id__exact=studentID)
-        projects = students[0].projects.all()
-        projectID = []
-        for project in projects:
-            projectID.append(project.id)
-
-        criteria1 = Q(student__exact=students[0].id)
-        criteria2 = Q(project__in=projectID)
-
-        grades = Grade.objects.filter(criteria1 & criteria2)
-        individualMarks = IndividualMark.objects.filter(criteria1 & criteria2)
-        jsonObj['id'] = students[0].id
-        jsonObj['name'] = students[0].name
-
-        jsonObj['projects'] = []
-        for project in projects:
-            temp = {}
-            tempIndividualMarks = []
-
-            for mark in individualMarks:
-                if mark.project.id == project.id:
-                    indMark = {'mark': mark.mark}
-                    tempIndividualMarks.append(indMark)
-
-            for grade in grades:
-                if grade.project.id == project.id:
-                    temp['grade'] = tempIndividualMarks
-                    temp['state'] = grade.state
-            temp['id'] = project.id
-            temp['name'] = project.name
-            temp['module'] = project.module.code
-
-            jsonObj['projects'].append(temp)
-        jsonObj['clerk_id'] = students[0].clerk_id
-
+        jsonObj = getStudentData(studentID, True)
         data = json.dumps(jsonObj, default=dumper, indent=2)
 
         return HttpResponse(data)
@@ -80,13 +93,24 @@ def academics(request):
     try:
         academicsID = request.GET['id']
     except:
-        academicsID = ''
+        return HttpResponse('{"error": "No academics ID provided"}')
 
-    if academicsID == '':
-        data = serializers.serialize('json', Academics.objects.all())
-    else:
-        data = serializers.serialize('json', Academics.objects.filter(clerk_id__exact=academicsID))
-    return HttpResponse(data)
+    if academicsID != '':
+        jsonObj = {}
+        academic = Academics.objects.filter(clerk_id__exact=academicsID)
+        if len(academic) == 0:
+            return HttpResponse({"error": "No academics with that ID"})
+        students = academic[0].students.all()
+        studentData = []
+        for stu in students:
+            studentData.append(getStudentData(stu.clerk_id, False))
+        jsonObj['id'] = academic[0].id
+        jsonObj['name'] = academic[0].name
+        jsonObj['students'] = studentData
+        jsonObj['clerk_id'] = academic[0].clerk_id
+
+        data = json.dumps(jsonObj, default=dumper, indent=2)
+        return HttpResponse(data)
 
 
 def convener(request):
